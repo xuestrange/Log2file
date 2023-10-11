81cd2782580b5142dcabe0ffa6d776b882cf91fb
xuestrange <xjz1cumt@163.com>
2023-10-11 22:09:47 +0800

    improve test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,8 @@
 using Log2file
 using Test
+Log2file.set_line_length!(40)
+Log2file.set_logfile_path!("./test/test.txt")
 function test_log2file()
-	Log2file.set_line_length!(40)
-	Log2file.set_logfile_path!("./test/test.txt")
-
 	@init_log
 	@section "Step 1: Data Generation"
 	a = [1, 2, 3]
