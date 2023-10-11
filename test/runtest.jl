b3896c5d56736acd6bde6f8a0bb18bb22617bfc0
xuestrange <xjz1cumt@163.com>
2023-10-11 21:29:56 +0800

    complete the test

--- test/runtest.jl
+++ test/runtest.jl
@@ -1,14 +1,12 @@
 using Log2file
 
-set_lline!(45)
-set_log_file!("./test/test.txt")
+Log2file.set_line_length!(40)
+Log2file.set_logfile_path!("./test/test.txt")
 
 @init_log
 @section "Step 1: Data Generation"
-
-a = 1
-b = "a"
-
-@log "1 ", "2 ", "b"
-@log "1", "2"
-@log  a, b
+a = [1, 2, 3]
+b = randn(2, 2)
+@log "a = $(a)"
+@log "a = $(a), b = $(b)"
+@section "END"
