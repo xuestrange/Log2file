394a8f1754241471a170153e35eb8893b952e7fb
xuestrange <xjz1cumt@163.com>
2023-10-11 22:01:06 +0800

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,19 @@
 using Log2file
 using Test
+function test_log2file()
+	Log2file.set_line_length!(40)
+	Log2file.set_logfile_path!("./test/test.txt")
 
+	@init_log
+	@section "Step 1: Data Generation"
+	a = [1, 2, 3]
+	b = randn(2, 2)
+	@log "a = $(a)"
+	@log "a = $(a), b = $(b)"
+	@section "END"
+	return true
+end
 @testset "Log2file.jl" begin
 	# Write your tests here.
-    @test try 
-        Log2file.set_line_length!(40)
-        Log2file.set_logfile_path!("./test/test.txt")
-
-        @init_log
-        @section "Step 1: Data Generation"
-        a = [1, 2, 3]
-        b = randn(2, 2)
-        @log "a = $(a)"
-        @log "a = $(a), b = $(b)"
-        @section "END"
-       true 
-    catch
-        false
-    end
+	@test test_log2file()
 end
