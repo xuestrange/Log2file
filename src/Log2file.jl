c8bfbd1ea82d51ed0739d2a5c3601e665fa47669
xuestrange <59390321+xuestrange@users.noreply.github.com>
2025-08-31 18:31:46 +0800

    Update Log2file.jl

--- src/Log2file.jl
+++ src/Log2file.jl
@@ -1,6 +1,6 @@
 module Log2file
 using Dates
 log_file = ""
-export @init_log, @section, @log, @logm, _LOGFILE_PATH, _LINE_LENGTH, _APPEND
+export @init_log, @section, @log, @logm
 include("utils.jl")
 end # module Log2file
