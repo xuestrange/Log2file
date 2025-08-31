d6a7ddc2bb995e7ea807b06341d03afc57775e96
xuestrange <59390321+xuestrange@users.noreply.github.com>
2025-08-31 17:10:01 +0800

    allow the append mode

--- src/Log2file.jl
+++ src/Log2file.jl
@@ -1,6 +1,6 @@
 module Log2file
 using Dates
 log_file = ""
-export @init_log, @section, @log, @logm, _LOGFILE_PATH, _LINE_LENGTH
+export @init_log, @section, @log, @logm, _LOGFILE_PATH, _LINE_LENGTH, _APPEND
 include("utils.jl")
 end # module Log2file
